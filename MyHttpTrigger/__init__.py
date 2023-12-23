d7768b04e440fa248e5825ef2482205f3ae33d87
Bart <Drbvr@users.noreply.github.com>
2023-12-23 21:30:40 +0100

    Delete MyHttpTrigger directory

--- MyHttpTrigger/__init__.py
+++
@@ -1,5 +0,0 @@
-import azure.functions as func
-
-def main(req: func.HttpRequest) -> func.HttpResponse:
-    html_content = "<html><body><h1>Hello from Azure Function!</h1></body></html>"
-    return func.HttpResponse(html_content, mimetype="text/html")
