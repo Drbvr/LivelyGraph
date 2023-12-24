0909fc30ecf6e58a2a448dd6475cb3bc28f8fa5a
Bart <Drbvr@users.noreply.github.com>
2023-12-24 22:02:02 +0100

    Update __init__.py

--- MyHttpTrigger/__init__.py
+++ MyHttpTrigger/__init__.py
@@ -1,5 +1,15 @@
 import azure.functions as func
+import os
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
-    html_content = "<html><body><h1>Hello from Azure Function!</h1></body></html>"
-    return func.HttpResponse(html_content, mimetype="text/html")
+    try:
+        # Assuming the HTML file is in the same directory as the function
+        with open(os.path.join(os.getcwd(), 'landing-page.html'), 'r') as file:
+            html_content = file.read()
+
+        return func.HttpResponse(html_content, mimetype="text/html")
+    except Exception as e:
+        return func.HttpResponse(
+             f"Error: {str(e)}",
+             status_code=500
+        )
